9154f5eb4b6973c1bfda2fd7c274af773f3974a3
Alyssa Butler <ca.butler10@gmail.com>
2018-07-30 10:11:57 +0200

    css renamed

--- src/withRoot.js
+++ src/withRoot.js
@@ -4,7 +4,7 @@ import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import getPageContext from "./getPageContext";
 
-import "./theme/app.css";
+import "./theme/App.css";
 
 function withRoot(Component) {
   class WithRoot extends React.Component {
