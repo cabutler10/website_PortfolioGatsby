a6b914eaf319f759308a1dc2c01732ec2deb5d78
Alyssa Butler <ca.butler10@gmail.com>
2022-06-14 17:35:49 +0200

    instal starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,62 +1,38 @@
-const config = require("./data/SiteConfig");
-
 module.exports = {
   siteMetadata: {
-    themeColor: `#d83a68`,
-    basePath: `/`,
-    title: config.siteTitle,
-    titleTemplate: "%s",
-    description: config.siteDescription,
-    siteUrl: config.siteUrl, // No trailing slash allowed!
-    image: "${__dirname}/src/assets/favicon/favicon.png", // Path to your image you placed in the 'static' folder
-    twitterUsername: "@f64c362be9384b6",
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `content`,
-        path: `${__dirname}/src/content`,
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `img`,
-        path: `${__dirname}/src/assets`,
-      },
-    },
-    `gatsby-plugin-mdx`,
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: config.siteGATrackingID
-    //   }
-    // },
-    // `gatsby-plugin-feed`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: config.siteTitle,
-        short_name: config.siteTitleShort,
-        start_url: "/",
-        background_color: "#f9f9f9",
-        theme_color: "#f7b64e",
-        display: "standalone",
-        icon: "src/assets/favicon/favicon.png", // This path is relative to the root of the site.
-        crossOrigin: `use-credentials`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-feed-mdx`,
-    `gatsby-plugin-sitemap`,
-    "gatsby-plugin-robots-txt",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-top-layout",
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
-};
+}
